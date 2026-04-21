3c831aa5c8797453ee5e83b86b892791c3c64fd6
Joel <eapenjoel4@gmail.com>
2026-04-21 19:53:37 +0530

    testing paused for now

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -8,11 +8,4 @@ describe('test suite :updateQuantity', () => {
     });
 });
 
-describe('test suite :updateDeliveryOption', () => {
-    it('for delivery option id 1', () => {
-        const deliverOption = updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", '2');
-        expect(deliverOption).toEqual('2');
-
-    })
-})
 
