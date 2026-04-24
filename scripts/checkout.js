190d300f1db848f76c857b7105e0e540b65dfd32
Joel <eapenjoel4@gmail.com>
2026-04-24 14:00:30 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import {renderPaymentSummary} from './checkout/payment-summary.js';
 //import '../data/cart-Class.js';
+import '../data/backend-practice.js';
 
 
 renderOrderSummary();
